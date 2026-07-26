70d7572050462a3d8d918d0ec89846062d6f172d
ken1988 <novilen.gov@gmail.com>
2026-07-27 05:09:56 +0900

    Fix map popup below map on mobile

--- trade/hako-main.php
+++ trade/hako-main.php
@@ -586,7 +586,9 @@ class Hako extends HakoIO {
 
     if($mode == 1 || $mode == 2) {
       print "<a href=\"javascript: void(0);\" onclick=\"ps($x,$y)\">";
-      $naviText = "{$comStr}\\n{$naviText}";
+      if($comStr !== '') {
+        $naviText = "{$comStr}\\n{$naviText}";
+      }
     }
     print "<img class=\"mapchip\" src=\"{$image}\" alt=\"{$point} {$naviTitle} {$comStr}\" onMouseOver=\"Navi({$naviPos},'{$image}', '{$naviTitle}', '{$point}', '{$naviText}', {$naviExp});\">";
 
